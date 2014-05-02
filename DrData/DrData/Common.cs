77b89afb1d0e17e2103d9544aa4873949ae2fc7a
drOpen <kudryashov.andrey@gmail.com>
2014-05-02 18:13:29 +0400

    [-] switch to 'ANY CPU' [-] header changes @ -> at

--- DrData/DrData/Common.cs
+++ DrData/DrData/Common.cs
@@ -21,7 +21,7 @@
 
       3. This notice may not be removed or altered from any source distribution.
 
-      Kudryashov Andrey <kudryashov.andrey@gmail.com>
+      Kudryashov Andrey <kudryashov.andrey at gmail.com>
 
  */
 

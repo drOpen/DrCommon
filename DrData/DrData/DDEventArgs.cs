0ebe0d21106bdbbf4f80051506e3c06bc184efeb
drOpen <kudryashov.andrey@gmail.com>
2015-08-30 02:58:38 +0300

    [+] add DDEventArgs dictonary support

--- DrData/DrData/DDEventArgs.cs
+++ DrData/DrData/DDEventArgs.cs
@@ -26,19 +26,137 @@
  */
 
 using System;
+using System.Collections;
+using System.Collections.Generic;
+
 namespace DrOpen.DrCommon.DrData
 {
-    public class DDEventArgs : EventArgs
+    public class DDEventArgs : EventArgs, IDictionary<string, DDNode>, IEnumerable<KeyValuePair<string, DDNode>>, ICollection<KeyValuePair<string, DDNode>>
     {
+        IDictionary<string, DDNode> args;
+
         #region DDEventArgs
-        public DDEventArgs() { }
-        public DDEventArgs(DDNode eventData)
+        public DDEventArgs()
         {
-            this.EventData = eventData;
+            this.args = new Dictionary<string, DDNode>();
+        }
+        public DDEventArgs(DDNode value): this((Guid.NewGuid().ToString()), value)
+        { }
+        public DDEventArgs(string key, DDNode value) : this()
+        {
+            this.args.Add(key, value);
+        }
+        public DDEventArgs(params KeyValuePair<string, DDNode>[] args)
+        {
+            foreach (KeyValuePair<string, DDNode> a in args)
+            {
+                this.args.Add(a);
+            }
         }
         #endregion DDEventArgs
 
         public DDNode EventData { get; set; }
 
+
+        public void Add(string key, DDNode value)
+        {
+            this.args.Add(key, value);
+        }
+
+        public bool ContainsKey(string key)
+        {
+            return this.args.ContainsKey(key);
+        }
+
+        public ICollection<string> Keys
+        {
+            get { return this.args.Keys; }
+        }
+
+        public bool Remove(string key)
+        {
+            return this.args.Remove(key);
+        }
+
+        public bool TryGetValue(string key, out DDNode value)
+        {
+            return this.args.TryGetValue(key, out value);
+        }
+
+        public ICollection<DDNode> Values
+        {
+            get { return this.args.Values; }
+        }
+
+        public DDNode this[string key]
+        {
+            get
+            {
+                return this.args[key];
+            }
+            set
+            {
+                this.args[key] = value;
+            }
+        }
+
+        public void Clear()
+        {
+            this.args.Clear();
+        }
+
+        public int Count
+        {
+            get { return this.args.Count; }
+        }
+
+        #region IEnumerable
+        /// <summary>
+        /// Returns an enumerator that iterates through the Attribute Collection (IEnumerator&lt;KeyValuePair&lt;string, DDNode&gt;&gt;).
+        /// </summary>
+        /// <returns>IEnumerator&lt;KeyValuePair&lt;string, DDNode&gt;&gt;</returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        /// <summary>
+        /// Returns an enumerator that iterates through the Attribute Collection (IEnumerator&lt;KeyValuePair&lt;string, DDNode&gt;&gt;).
+        /// </summary>
+        /// <returns>IEnumerator&lt;KeyValuePair&lt;string, DDNode&gt;&gt;</returns>
+        public virtual IEnumerator<KeyValuePair<string, DDNode>> GetEnumerator()
+        {
+            foreach (var arg in this.args)
+            {
+                yield return arg;
+            }
+        }
+
+        #endregion IEnumerable
+
+
+        public void Add(KeyValuePair<string, DDNode> item)
+        {
+            this.args.Add(item);
+        }
+
+        public bool Contains(KeyValuePair<string, DDNode> item)
+        {
+            return this.args.Contains(item);
+        }
+
+        public void CopyTo(KeyValuePair<string, DDNode>[] array, int arrayIndex)
+        {
+            this.args.CopyTo(array, arrayIndex);
+        }
+
+        public bool IsReadOnly
+        {
+            get { return this.args.IsReadOnly; }
+        }
+
+        public bool Remove(KeyValuePair<string, DDNode> item)
+        {
+            return this.args.Remove(item);
+        }
     }
 }

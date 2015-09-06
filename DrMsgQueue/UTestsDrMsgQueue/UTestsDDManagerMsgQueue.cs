025f1001ff2805c919b3d4dbe382220ad78a2abb
drOpen <kudryashov.andrey@gmail.com>
2015-09-06 22:02:17 +0300

    [+] unit test for DDManagerMsgQueue

---
+++ DrMsgQueue/UTestsDrMsgQueue/UTestsDDManagerMsgQueue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DrOpen.DrCommon.DrData ;
+using DrOpen.DrCommon.DrMsgQueue;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace UTestsDrMsgQueue
+{
+    [TestClass]
+    public class UTestsDDManagerMsgQueue
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("Start test");
+            var ddQ = new DDManagerMsgQueue();
+            for (int i = 1; i < 1000; i++)
+            {
+                ddQ.Put(new DDNode());
+            }
+
+        }
+    }
+}

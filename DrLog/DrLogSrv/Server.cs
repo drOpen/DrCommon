413dc4c47e1ea39f09518e153ad14b5e8a131241
drOpen <kudryashov.andrey@gmail.com>
2015-11-26 19:13:21 +0300

    [+] add Server

---
+++ DrLog/DrLogSrv/Server.cs
@@ -0,0 +1,13 @@
+﻿using DrOpen.DrCommon.DrMsgQueue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DrOpen.DrCommon.DrLog.DrLogSrv
+{
+    public class Server : DDManagerMsgQueue
+    {
+
+    }
+}

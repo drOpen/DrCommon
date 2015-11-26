213408c9265cb23f4d35c4994e4d63668abc5e53
drOpen <kudryashov.andrey@gmail.com>
2015-11-26 19:12:05 +0300

    [.] add log Providers/ILogProvider.cs

---
+++ DrLog/DrLogSrv/Providers/LogConsole.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DrOpen.DrCommon.DrLog.DrLogSrv.Providers
+{
+    public class LogConsole : LogProvider
+    {
+
+        public LogConsole(DrData.DDNode config)
+            : base(config)
+        { }
+
+        public override void Write(DrData.DDNode msg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Name
+        {
+            get 
+            { 
+                throw new NotImplementedException(); 
+            }
+        }
+    }
+}
---
+++ DrLog/DrLogSrv/Providers/LogProvider.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using DrOpen.DrCommon.DrData;
+
+namespace DrOpen.DrCommon.DrLog.DrLogSrv.Providers
+{
+    public abstract class LogProvider: ILogProvider
+    {
+
+        public LogProvider(DDNode config)
+        {
+            Config = config;
+        }
+
+        public abstract void Write(DDNode msg);
+        public abstract void Dispose();
+
+        public abstract string Name { get; }
+
+        virtual public DDNode Config {get; set;}
+
+        virtual public DDType ConfigNodeType
+        {
+            get { return Config.Type ; }
+        }
+
+        #region basic attributes for messages
+        /// <summary>
+        /// creation time of the message 
+        /// </summary>
+        public const string AttDateTime = "DateTime";
+        /// <summary>
+        /// body of message
+        /// </summary>
+        public const string AttBody = "Body";
+        /// <summary>
+        /// Log level of message
+        /// </summary>
+        public const string AttLogLevel = "LogLevel";
+        /// <summary>
+        /// Who created the message 
+        /// </summary>
+        public const string AttSource = "LogLevel";
+        /// <summary>
+        /// The list of providers who will be read this message. by default all providers
+        /// </summary>
+        public const string AttProviders = "Providers";
+        /// <summary>
+        /// The list of recipients who will be receive this message. by default all recipients
+        /// </summary>
+        public const string AttRecipients = "Providers";
+        #endregion basic attributes for messages
+
+
+    }
+}

c5e0496de955646978ce01604ee656d49af3379a
drOpen <kudryashov.andrey@gmail.com>
2016-01-18 16:34:02 +0300

    [+] add providers

---
+++ DrLog/DrLogSrv/Providers/LogFile.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DrOpen.DrCommon.DrData;
+
+namespace DrOpen.DrCommon.DrLog.DrLogSrv.Providers
+{
+    public class LogFile : Provider
+    {
+
+        public LogFile(DDNode config)
+            : base(config)
+        { }
+        public LogFile(DDNode config, bool mergeWithDefault)
+            : base(config, mergeWithDefault)
+        { }
+
+        public override void Write(DDNode msg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public override DDNode DefaultConfig
+        {
+            get
+            {
+                return GetDefaultConfig();
+            }
+        }
+
+
+        public static DDNode GetDefaultConfig()
+        {
+            var n = GetCommonConfig(typeof(LogFile).AssemblyQualifiedName);
+            n.Attributes.Add(AttDateTimeFormat, String.Empty);
+            n.Attributes.Add(AttColumnSeparator, "\t");
+            n.Attributes.Add(AttFileName, "Log.log");
+            n.Attributes.Add(AttReopenFileHandle, false);
+            //n.Attributes.Add();
+            return n;
+        }
+
+        #region basic attributes for config
+        /// <summary>
+        /// Converts the value of the current DateTime to its equivalent string representation using the specified culture-specific format information.
+        /// </summary>
+        public const string AttDateTimeFormat = "DateTimeFormat";
+        /// <summary>
+        /// Column separator
+        /// </summary>
+        public const string AttColumnSeparator = "ColumnSeparator";
+        /// <summary>
+        /// File name
+        /// </summary>
+        public const string AttFileName = "FileName";
+        /// <summary>
+        /// reopen the handle at each message
+        /// </summary>
+        public const string AttReopenFileHandle = "ReopenFileHandle";
+
+        #endregion basic attributes for config
+
+    }
+}
---
+++ DrLog/DrLogSrv/Providers/Provider.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using DrOpen.DrCommon.DrData;
+
+namespace DrOpen.DrCommon.DrLog.DrLogSrv.Providers
+{
+    public abstract class Provider : IProvider
+    {
+
+        #region Provider
+        /// <summary>
+        /// create provider with specified configuration
+        /// </summary>
+        /// <param name="config">provider configuration</param>
+        public Provider(DDNode config)
+            : this(config, false)
+        { }
+        /// <summary>
+        /// create provider with specified configuration and merge with default configuration for this provider
+        /// </summary>
+        /// <param name="config">provider configuration</param>
+        /// <param name="mergeWithDefault">specify true for merge with default configuration for this provider</param>
+        public Provider(DDNode config, bool mergeWithDefault)
+        {
+            if (config == null) throw new ApplicationException("The provider configuration cannot be null.");
+            if (config.Type != Type) throw new ApplicationException(string.Format("The type '{0}' of provider configuration is not supported. The expected type is '{1}'.", config.Type, Type));
+            if ((mergeWithDefault) || (DefaultConfig != null))
+            {
+                Config = DefaultConfig;
+                Config.Merge(config, DDNode.DDNODE_MERGE_OPTION.ALL, ResolveConflict.OVERWRITE);
+            }
+            else
+            {
+                Config = config;
+            }
+            RebuildConfiguration();
+        }
+        #endregion Provider
+
+        public abstract void Write(DDNode msg);
+        public abstract void Dispose();
+
+        /// <summary>
+        /// Provider name. By default is equls to provider Type
+        /// </summary>
+        virtual public string Name
+        {
+            get { return Type.ToString(); }
+        }
+        /// <summary>
+        /// curent configuration
+        /// </summary>
+        virtual public DDNode Config
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// default configuration for this provider
+        /// </summary>
+        public abstract DDNode DefaultConfig { get; }
+
+        virtual public DDType Type
+        {
+            get { return this.GetType().Name; }
+        }
+
+        virtual public DrLogSrv.LogLevel Level
+        { get; set; }
+        virtual public DrLogSrv.LogExceptionLevel ExceptionLevel
+        { get; set; }
+        virtual public void RebuildConfiguration()
+        {
+            this.Level = (DrLogSrv.LogLevel)Enum.Parse(typeof(DrLogSrv.LogLevel), Config.Attributes.GetValue(DrLogProviderConst.AttLevel, DefaultLevel), true);
+            this.ExceptionLevel = (DrLogSrv.LogExceptionLevel)Enum.Parse(typeof(DrLogSrv.LogExceptionLevel), Config.Attributes.GetValue(DrLogProviderConst.AttExceptionLevel, DrLogSrv.LogExceptionLevel.ALL), true);
+        }
+        /// <summary>
+        /// default log level
+        /// </summary>
+        public const DrLogSrv.LogLevel DefaultLevel = LogLevel.ALL;
+        /// <summary>
+        /// default log exception level
+        /// </summary>
+        public const DrLogSrv.LogExceptionLevel DefaultExceptionLevel = LogExceptionLevel.ALL;
+
+        /// <summary>
+        /// returns configuration provider node with common attributes: 'AttLevel=All', 'AttExceptionLevel=All'
+        /// </summary>
+        /// <returns></returns>
+        public static DDNode GetCommonConfig(DDType type)
+        {
+            var n = new DDNode(type);
+            n.Attributes.Add(DrLogProviderConst.AttLevel, DefaultLevel.ToString());
+            n.Attributes.Add(DrLogProviderConst.AttExceptionLevel, DefaultExceptionLevel.ToString());
+            return n;
+        }
+    }
+}

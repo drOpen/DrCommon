c2b0697919200546dee33462ed9425cb754ccfdd
dropen <kudryashov.andrey@gmail.com>
2017-08-04 13:53:30 +0300

    [+] added extension for DDValue

---
+++ DrData/DrDataEx/DDValueEx.cs
@@ -0,0 +1,43 @@
+﻿/*
+  DDValueEx.cs -- extensions for 'DrData.DDValue' 1.0, July 22, 2017
+ 
+  Copyright (c) 2013-2017 Kudryashov Andrey aka Dr
+ 
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+      1. The origin of this software must not be misrepresented; you must not
+      claim that you wrote the original software. If you use this software
+      in a product, an acknowledgment in the product documentation would be
+      appreciated but is not required.
+
+      2. Altered source versions must be plainly marked as such, and must not be
+      misrepresented as being the original software.
+
+      3. This notice may not be removed or altered from any source distribution.
+
+      Kudryashov Andrey <kudryashov.andrey at gmail.com>
+
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Security;
+
+namespace DrOpen.DrCommon.DrData
+{
+    public static class DDValueEx
+    {
+        public static SecureString ToSecureString(this DDValue v)
+        {
+            var sc = new SecureString();
+            foreach (char c in v.GetValueAsString()) sc.AppendChar(c);
+            return sc;
+        }
+    }
+}
--- DrLog/UTests/UTestDrLogSrv/UTestProviderLogFile.cs
+++ DrLog/UTests/UTestDrLogSrv/UTestProviderLogFile.cs
@@ -15,18 +15,18 @@ namespace UTestDrLogSrv
         public void TestMethod1()
         {
             var logFile = new LogFile(new DDNode(new DDType (typeof(LogFile).Name)), true);
-            logFile.Write(Logger.MessageItem(DateTime.Now, LogLevel.INF , "Source", null, "Start", null , null));
+            logFile.Write(Logger.MessageItem(DateTime.Now, LogLevel.INF , "Source", null, null, "Start", null , null));
             try
             {
                 throw new DivideByZeroException("Divide by zero");
             }
             catch (Exception e)
             {
-                logFile.Write(Logger.MessageItem(DateTime.Now, DrOpen.DrCommon.DrLog.DrLogClient.LogLevel.ERR, "source", new Exception("ExceptionBody", e), "body", null, null));
+                logFile.Write(Logger.MessageItem(DateTime.Now, DrOpen.DrCommon.DrLog.DrLogClient.LogLevel.ERR, "source", null, new Exception("ExceptionBody", e), "body", null, null));
             }
-            logFile.Write(Logger.MessageItem(DateTime.Now, LogLevel.TRC, "Source", null, "Trace", null, null));
-            logFile.Write(Logger.MessageItem(DateTime.Now, LogLevel.DBG, "Source", null, "Debug", null, null));
-            logFile.Write(Logger.MessageItem(DateTime.Now, LogLevel.INF, "Source", null, "Stop", null, null));
+            logFile.Write(Logger.MessageItem(DateTime.Now, LogLevel.TRC, "Source", null, null, "Trace", null, null));
+            logFile.Write(Logger.MessageItem(DateTime.Now, LogLevel.DBG, "Source", null, null, "Debug", null, null));
+            logFile.Write(Logger.MessageItem(DateTime.Now, LogLevel.INF, "Source", null, null, "Stop", null, null));
         }
     }
 }

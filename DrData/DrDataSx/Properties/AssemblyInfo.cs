27afcea907c0b847c3e070053ba1ff33f2833e41
dropen <kudryashov.andrey@gmail.com>
2019-02-05 21:23:16 +0300

    [-] fix the incorrect namespace name DrDataSe -> DrDataSx

--- DrData/DrDataSx/Properties/AssemblyInfo.cs
+++ DrData/DrDataSx/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DrDataSe")]
+[assembly: AssemblyTitle("DrDataSx")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DrDataSe")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("DrDataSx")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
